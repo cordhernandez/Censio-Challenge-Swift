b0b1aa2614f4baa7ff034606f301fdf638906d3b
Cordero <cord.a.hernandez@gmail.com>
2018-05-16 10:21:38 -0700

    Adds Xcode Project

---
+++ Censio Challenge Swift/Censio Challenge Swift/Censio_Challenge_Swift.h
@@ -0,0 +1,19 @@
+//
+//  Censio_Challenge_Swift.h
+//  Censio Challenge Swift
+//
+//  Created by Cordero Hernandez on 5/16/18.
+//  Copyright © 2018 Cordero Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Censio_Challenge_Swift.
+FOUNDATION_EXPORT double Censio_Challenge_SwiftVersionNumber;
+
+//! Project version string for Censio_Challenge_Swift.
+FOUNDATION_EXPORT const unsigned char Censio_Challenge_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Censio_Challenge_Swift/PublicHeader.h>
+
+
